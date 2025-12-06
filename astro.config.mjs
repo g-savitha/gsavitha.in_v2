198c0f3574677900d118e331e27300aac86ae916
Savitha <gsavitha@protonmail.com>
2025-12-07 05:24:24 +0530

    update blog ui and fix minor bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,31 @@ import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), mdx()]
+  integrations: [
+    react(),
+    expressiveCode({
+      themes: ['dracula'],
+      styleOverrides: {
+        codeFontFamily: "'Source Code Pro', monospace",
+        uiFontFamily: "'Comic Neue', sans-serif",
+        frames: {
+          frameBoxShadowCssValue: '4px 4px 0px #000',
+          editorActiveTabBorderColor: '#000',
+          editorTabBorderRadius: '0px',
+        },
+        borderColor: '#000',
+        borderRadius: '0px',
+        borderWidth: '2px',
+      }
+    }),
+    mdx()
+  ]
 });
